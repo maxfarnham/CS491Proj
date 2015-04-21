67745aa1529c9ddda8773de363af7b244a1e20ae
maxfarnham <max.farnham@vizwit.com>
2015-04-21 10:57:55 -0500

    LDA bug fixes

--- pos.py
+++ pos.py
@@ -1,7 +1,15 @@
 import nltk, string
 
-def get_sentence_tags(sentence): 
-    return [x[1] for x in nltk.pos_tag(nltk.word_tokenize(sentence.translate(string.maketrans("",""), string.punctuation) ))    ]
+NOUNS = ['NN', 'NNS', 'NNP', 'NNPS']
+
+def tag_sentence(sentence): 
+	return nltk.pos_tag(sentence)
+
+def get_sentences(text):
+	# lazy load sentence detector
+	sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+	return sent_detector.tokenize(text)
 
 def get_nouns(sentence): 
-    return [x[0] if x[1] in ['NN','NNS','NNP','NNPS'] else '' for x in nltk.pos_tag(nltk.word_tokenize(sentence.translate(string.maketrans("",""), string.punctuation) ))    ]
+	tagged_sentence = tag_sentence(sentence)
+	return [word if pos in NOUNS else '' for word,pos in tagged_sentence]
